11a1f4b298a6c9a3f211121278379689e030d313
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-09-21 19:37:26 +0900

    Regexのテストケースを追加

---
+++ test/com/m12i/regex/RegexTest.java
@@ -0,0 +1,102 @@
+package com.m12i.regex;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+public class RegexTest {
+
+	@Test
+	public void matchesTest00() {
+		final Regex re0 = Regex.compile("");
+		assertThat(re0.pattern, is(""));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest01() {
+		final Regex re0 = Regex.compile("a");
+		assertThat(re0.pattern, is("a"));
+		assertThat(re0.matches("a"), is(true));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(false));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest02() {
+		final Regex re0 = Regex.compile("ab");
+		assertThat(re0.pattern, is("ab"));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("a"), is(false));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest03() {
+		final Regex re0 = Regex.compile("ab*");
+		assertThat(re0.pattern, is("ab*"));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("abb"), is(true));
+		assertThat(re0.matches("a"), is(true));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest04() {
+		final Regex re0 = Regex.compile("ab+");
+		assertThat(re0.pattern, is("ab+"));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("abb"), is(true));
+		assertThat(re0.matches("a"), is(false));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest05() {
+		final Regex re0 = Regex.compile("a[b-z]");
+		assertThat(re0.pattern, is("a[b-z]"));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("abb"), is(false));
+		assertThat(re0.matches("az"), is(true));
+		assertThat(re0.matches("a"), is(false));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest06() {
+		final Regex re0 = Regex.compile("a[b-z]*");
+		assertThat(re0.pattern, is("a[b-z]*"));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("abb"), is(true));
+		assertThat(re0.matches("az"), is(true));
+		assertThat(re0.matches("a"), is(true));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+	@Test
+	public void matchesTest07() {
+		final Regex re0 = Regex.compile("a[b-z]+");
+		assertThat(re0.pattern, is("a[b-z]+"));
+		assertThat(re0.matches("aa"), is(false));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("abb"), is(true));
+		assertThat(re0.matches("az"), is(true));
+		assertThat(re0.matches("a"), is(false));
+		assertThat(re0.matches("b"), is(false));
+		assertThat(re0.matches(""), is(false));
+	}
+
+}
