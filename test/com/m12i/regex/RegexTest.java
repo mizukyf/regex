7c44d92405a7e1b7fccf29ac42572e6bdb9ab84d
mizuky fujitani <mizuki.fujitani@gmail.com>
2014-09-22 14:19:03 +0900

    UNIONに関するテストコードを追加

--- test/com/m12i/regex/RegexTest.java
+++ test/com/m12i/regex/RegexTest.java
@@ -141,4 +141,93 @@ public class RegexTest {
 		assertThat(re0.matches("b"), is(false));
 		assertThat(re0.matches(""), is(false));
 	}
+
+	@Test
+	public void matchesTest20() {
+		final Regex re0 = Regex.compile("(a|b)");
+		assertThat(re0.matches("a"), is(true));
+		assertThat(re0.matches("b"), is(true));
+		assertThat(re0.matches("c"), is(false));
+	}
+
+	@Test
+	public void matchesTest21() {
+		final Regex re0 = Regex.compile("(a|b|c)");
+		assertThat(re0.matches("a"), is(true));
+		assertThat(re0.matches("b"), is(true));
+		assertThat(re0.matches("c"), is(true));
+		assertThat(re0.matches("d"), is(false));
+	}
+
+	@Test
+	public void matchesTest22() {
+		final Regex re0 = Regex.compile("a(a|b)");
+		assertThat(re0.matches("aa"), is(true));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("ac"), is(false));
+	}
+
+	@Test
+	public void matchesTest23() {
+		final Regex re0 = Regex.compile("(a|b)a");
+		assertThat(re0.matches("aa"), is(true));
+		assertThat(re0.matches("ba"), is(true));
+		assertThat(re0.matches("ca"), is(false));
+	}
+
+	@Test
+	public void matchesTest24() {
+		final Regex re0 = Regex.compile("(aa|ab)");
+		assertThat(re0.matches("aa"), is(true));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("ac"), is(false));
+	}
+
+	@Test
+	public void matchesTest25() {
+		final Regex re0 = Regex.compile("(aa|ab|ac)");
+		assertThat(re0.matches("aa"), is(true));
+		assertThat(re0.matches("ab"), is(true));
+		assertThat(re0.matches("ac"), is(true));
+		assertThat(re0.matches("ad"), is(false));
+	}
+
+	@Test
+	public void matchesTest26() {
+		final Regex re0 = Regex.compile("a(aa|ab)");
+		assertThat(re0.matches("aaa"), is(true));
+		assertThat(re0.matches("aab"), is(true));
+		assertThat(re0.matches("aac"), is(false));
+	}
+
+	@Test
+	public void matchesTest27() {
+		final Regex re0 = Regex.compile("(aa|ab)a");
+		assertThat(re0.matches("aaa"), is(true));
+		assertThat(re0.matches("aba"), is(true));
+		assertThat(re0.matches("aca"), is(false));
+	}
+
+	@Test
+	public void matchesTest30() {
+		final Regex re0 = Regex.compile("(a|b)*c");
+		assertThat(re0.matches("ac"), is(true));
+		assertThat(re0.matches("bc"), is(true));
+		assertThat(re0.matches("c"), is(true));
+		assertThat(re0.matches("cc"), is(false));
+	}
+
+	@Test
+	public void matchesTest31() {
+		final Regex re0 = Regex.compile("(a|b)+c");
+		assertThat(re0.matches("ac"), is(true));
+		assertThat(re0.matches("bc"), is(true));
+		assertThat(re0.matches("c"), is(false));
+		assertThat(re0.matches("cc"), is(false));
+		assertThat(re0.matches("aac"), is(true));
+		assertThat(re0.matches("abc"), is(true));
+		assertThat(re0.matches("bac"), is(true));
+		assertThat(re0.matches("bbc"), is(true));
+	}
+
 }
